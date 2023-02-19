d060ed68d3ec10f261fec97f4d7ffc789b06d4c0
BrunoUemura <bruno.h.uemura@outlook.com>
2023-02-19 20:53:39 -0300

    feat: adds scraper service

---
+++ apps/scraper-service/cmd/scraper.go
@@ -0,0 +1,78 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"sync"
+
+	"github.com/gocolly/colly"
+)
+
+type StockMarketPrice struct {
+	Ticker string `json:"ticker"`
+	Price float64 `json:"price"`
+}
+
+func (c *StockMarketPrice) String() string {
+    return fmt.Sprintf(`{"ticker": "%s", "price": %.2f}`, c.Ticker, c.Price)
+}
+
+func StartScraping(stocks []string) {
+	ch := make(chan string)
+	var wg sync.WaitGroup
+
+	for _, stock := range stocks {
+		wg.Add(1)
+		go stockCurrentPrice(stock, ch, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for stockData := range ch {
+		fmt.Println(stockData)
+	}
+}
+
+func stockCurrentPrice(stock string, ch chan string, wg *sync.WaitGroup) {
+	defer (*wg).Done()
+	
+	c := colly.NewCollector(
+	  colly.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"),
+	  colly.AllowedDomains("finance.yahoo.com"),
+	  colly.MaxBodySize(0),
+	  colly.AllowURLRevisit(),
+	  colly.Async(true),
+	)
+  
+	// Set max Parallelism and introduce a Random Delay
+	c.Limit(&colly.LimitRule{
+	  DomainGlob:  "*",
+	  Parallelism: 2,
+	})
+	c.OnRequest(func(r *colly.Request) {
+	  log.Println("Visiting", r.URL.String())
+	})
+  
+	c.OnHTML(`fin-streamer[data-field="regularMarketPrice"]`, func(e *colly.HTMLElement) {
+	  symbol := e.Attr("data-symbol")
+	  if symbol != stock {
+		return
+	  }
+	  
+	  value := e.Attr("value")
+	  price, _ := strconv.ParseFloat(value, 64)
+	  stockMarketPrice := &StockMarketPrice{
+		Ticker: stock,
+		Price: price, 
+	  }
+
+	  ch <- stockMarketPrice.String()
+	})
+  
+	c.Visit("https://finance.yahoo.com/quote/"+stock)
+	c.Wait()
+  }
--- apps/scraper-service/main.go
+++ apps/scraper-service/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"scraper-service/cmd"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello from Scraper Service")
+  start := time.Now()
+  stocks := []string {
+    "AAPL",
+    "MSFT",
+    "AMZN",
+	"MXRF11.SA",
+  }
+  
+  cmd.StartScraping(stocks)
+  fmt.Println("Completed the code process, took: %f seconds", time.Since(start).Seconds())
 }
